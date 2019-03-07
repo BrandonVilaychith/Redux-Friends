3f800dfd5ea6c00e67ce554d90167e1677864119
Brandon Vilaychith <blvilaychith@gmail.com>
2019-03-07 12:19:15 -0600

    updated

--- friends/src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import { friendsReducer } from './friendsReducer';
-import { loadingReducer } from './loadingReducer';
-
-export default combineReducers({
-  friends: friendsReducer,
-  fetching: loadingReducer
-});
